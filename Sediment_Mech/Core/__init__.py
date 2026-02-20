1bda22802b6285112aa60d13f26f93b4002ccc9d
carlocamporeale76-polito <carlo.camporeale76@gmail.com>
2026-02-20 23:18:09 +0100

    Update __init__.py

--- Sediment_Mech/Core/__init__.py
+++ Sediment_Mech/Core/__init__.py
@@ -1,1 +1,19 @@
+# Expose selected core functions for simple imports like:
+# from Sediment_Mech.core.io import read_psd_surface_csv
+from . import io
+from . import active_layer
+from . import parker1990
+from . import wilcock_crowe2003
+from . import ashida_michiue
+from . import annual
+from . import transport_common
+from . import hydraulics
+
+# re-export commonly used symbols
+from .io import read_gsd_xlsx, read_psd_surface_csv, read_fdc_csv
+from .active_layer import geometric_mean_Dsg, sorting_sigma_s_log2
+from .parker1990 import compute_parker1990
+from .wilcock_crowe2003 import compute_wilcock_crowe2003
+from .ashida_michiue import compute_ashida_michiue
+from .annual import compute_annual_bedload
 
